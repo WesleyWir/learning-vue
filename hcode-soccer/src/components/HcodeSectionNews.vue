563b845623575d5d707c4505d2dccd24f1f30dfa
Wesley Wirmond <wesleywirmond@gmail.com>
2021-02-20 23:11:38 -0300

    Component Props vue-cli

--- hcode-soccer/src/components/HcodeSectionNews.vue
+++ hcode-soccer/src/components/HcodeSectionNews.vue
@@ -1,9 +1,41 @@
 <template>
         <section>
             <div class="container">
-                <HcodeSectionNewsIndividual />
-                <HcodeSectionNewsIndividual />
-                <HcodeSectionNewsIndividual />
+                <HcodeSectionNewsIndividual 
+                    img-name="news1.jpg"
+                    img-info = "Notícia 1"
+                    news-title="Começam os treinos para a nova temporada"
+                    news-content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error soluta 
+                    quibusdam quia esse iure tenetur nisi? Sint maiores reprehenderit nesciunt vel, maxime 
+                    consectetur earum sequi, voluptates ab eos ullam provident? Lorem ipsum, dolor sit amet 
+                    consectetur adipisicing elit. Consectetur debitis vero dolores veniam neque, architecto ab 
+                    maxime odit ut. Perferendis quaerat explicabo eos nihil cumque sed, itaque incidunt quisquam aliquid!"
+                    news-date="01/01/2020"
+                />
+
+                <HcodeSectionNewsIndividual 
+                    img-name="news2.jpg"
+                    img-info = "Notícia 2"
+                    news-title="Jogo de quarta-feira termina empatado"
+                    news-content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error soluta 
+                    quibusdam quia esse iure tenetur nisi? Sint maiores reprehenderit nesciunt vel, maxime 
+                    consectetur earum sequi, voluptates ab eos ullam provident? Lorem ipsum, dolor sit amet 
+                    consectetur adipisicing elit. Consectetur debitis vero dolores veniam neque, architecto ab 
+                    maxime odit ut. Perferendis quaerat explicabo eos nihil cumque sed, itaque incidunt quisquam aliquid!"
+                    news-date="07/01/2020"
+                />
+
+                <HcodeSectionNewsIndividual 
+                    img-name="news3.jpg"
+                    img-info = "Notícia 3"
+                    news-title="A inauguração do novo estádio"
+                    news-content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error soluta 
+                    quibusdam quia esse iure tenetur nisi? Sint maiores reprehenderit nesciunt vel, maxime 
+                    consectetur earum sequi, voluptates ab eos ullam provident? Lorem ipsum, dolor sit amet 
+                    consectetur adipisicing elit. Consectetur debitis vero dolores veniam neque, architecto ab 
+                    maxime odit ut. Perferendis quaerat explicabo eos nihil cumque sed, itaque incidunt quisquam aliquid!"
+                    news-date="20/01/2020"
+                />
             </div>
         </section>
 </template>
--- hcode-soccer/src/components/HcodeSectionNewsIndividual.vue
+++ hcode-soccer/src/components/HcodeSectionNewsIndividual.vue
@@ -1,21 +1,42 @@
 <template>
     <div class="row">
         <div class="col-3">
-            <img src="../assets/news1.jpg" alt="Notícia 1">
+            <img :src="require('../assets/' + this.imgName)" :alt="imgInfo">
         </div>
         <div class="col-9">
-            <h2>Começam os treinos para a nova temporada</h2>
+            <h2>{{ newsTitle }}</h2>
             
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error soluta quibusdam quia esse iure tenetur nisi? Sint maiores reprehenderit nesciunt vel, maxime consectetur earum sequi, voluptates ab eos ullam provident? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur debitis vero dolores veniam neque, architecto ab maxime odit ut. Perferendis quaerat explicabo eos nihil cumque sed, itaque incidunt quisquam aliquid!</p>
+            <p>{{ newsContent | truncate(200) }}</p>
 
-            <span class="font-italic">01/01/2020</span>
+            <span class="font-italic">{{ newsDate }}</span>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    
+    props: {
+        imgName:{
+            type: String,
+            required: true,
+        },
+        imgInfo:{
+            type: String,
+            required: true,
+        },
+        newsTitle:{
+            type: String,
+            required: true
+        },
+        newsContent:{
+            type: String,
+            required: true
+        },
+        newsDate:{
+            type: String,
+            required: true
+        }
+    }
 }
 </script>
 
