b2c111f765b509e718c79cabe0ac621233a51aff
Wesley Wirmond <wesleywirmond@gmail.com>
2021-02-21 15:04:22 -0300

    Getters part 2  Vuex

--- hcode-soccer/src/components/HcodeSectionNews.vue
+++ hcode-soccer/src/components/HcodeSectionNews.vue
@@ -20,6 +20,7 @@
 
 <script>
 import HcodeSectionNewsIndividual from './HcodeSectionNewsIndividual';
+import { mapGetters } from 'vuex'
 
 export default {
     components:{
@@ -27,34 +28,14 @@ export default {
     },
     data() {
         return {
-            news: []
+           
         }
     },
-    created() {
-        this.news = [{
-            id: 1,
-            title: 'Começam os treinos para a nova temporada',
-            content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error soluta quibusdam quia esse iure tenetur nisi? Sint maiores reprehenderit nesciunt vel, maxime consectetur earum sequi, voluptates ab eos ullam provident? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur debitis vero dolores veniam neque, architecto ab maxime odit ut. Perferendis quaerat explicabo eos nihil cumque sed, itaque incidunt quisquam aliquid!',
-            date: '2020-01-01',
-            img: 'news1.jpg',
-            imgInfo: 'Notícia 1'
-
-        },{
-            id: 2,
-            title: 'Jogo de quarta-feira termina empatado',
-            content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error soluta quibusdam quia esse iure tenetur nisi? Sint maiores reprehenderit nesciunt vel, maxime consectetur earum sequi, voluptates ab eos ullam provident? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur debitis vero dolores veniam neque, architecto ab maxime odit ut. Perferendis quaerat explicabo eos nihil cumque sed, itaque incidunt quisquam aliquid!',
-            date: '2020-01-07',
-            img: 'news2.jpg',
-            imgInfo: 'Notícia 2'
-        }, {
-            id: 3,
-            title: 'A inauguração do novo estádio',
-            content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error soluta quibusdam quia esse iure tenetur nisi? Sint maiores reprehenderit nesciunt vel, maxime consectetur earum sequi, voluptates ab eos ullam provident? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur debitis vero dolores veniam neque, architecto ab maxime odit ut. Perferendis quaerat explicabo eos nihil cumque sed, itaque incidunt quisquam aliquid!',
-            date: '2020-01-22',
-            img: 'news3.jpg',
-            imgInfo: 'Notícia 3'
-        }]
-    },
+    computed:{
+        ...mapGetters({
+            news: 'getNews'
+        })
+    }
 }
 </script>
 
--- hcode-soccer/src/store/store.js
+++ hcode-soccer/src/store/store.js
@@ -7,6 +7,29 @@ export default new Vuex.Store({
     state:{
         championship: 'Campeonato Brasileiro',
         clubName: "Flamengo",
+        news: [{
+            id: 1,
+            title: 'Começam os treinos para a nova temporada',
+            content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error soluta quibusdam quia esse iure tenetur nisi? Sint maiores reprehenderit nesciunt vel, maxime consectetur earum sequi, voluptates ab eos ullam provident? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur debitis vero dolores veniam neque, architecto ab maxime odit ut. Perferendis quaerat explicabo eos nihil cumque sed, itaque incidunt quisquam aliquid!',
+            date: '2020-01-01',
+            img: 'news1.jpg',
+            imgInfo: 'Notícia 1'
+
+        },{
+            id: 2,
+            title: 'Jogo de quarta-feira termina empatado',
+            content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error soluta quibusdam quia esse iure tenetur nisi? Sint maiores reprehenderit nesciunt vel, maxime consectetur earum sequi, voluptates ab eos ullam provident? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur debitis vero dolores veniam neque, architecto ab maxime odit ut. Perferendis quaerat explicabo eos nihil cumque sed, itaque incidunt quisquam aliquid!',
+            date: '2020-01-07',
+            img: 'news2.jpg',
+            imgInfo: 'Notícia 2'
+        }, {
+            id: 3,
+            title: 'A inauguração do novo estádio',
+            content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error soluta quibusdam quia esse iure tenetur nisi? Sint maiores reprehenderit nesciunt vel, maxime consectetur earum sequi, voluptates ab eos ullam provident? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur debitis vero dolores veniam neque, architecto ab maxime odit ut. Perferendis quaerat explicabo eos nihil cumque sed, itaque incidunt quisquam aliquid!',
+            date: '2020-01-22',
+            img: 'news3.jpg',
+            imgInfo: 'Notícia 3'
+        }]
     },
     getters: {
         getChampionship(state){
@@ -14,6 +37,9 @@ export default new Vuex.Store({
         },
         getClubName(state){
             return state.clubName;
+        },
+        getNews(state){
+            return state.news;
         }
     }
 })
