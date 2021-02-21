d33b702cd95be75b9c2012bed52f07fdd66c2fa4
Wesley Wirmond <wesleywirmond@gmail.com>
2021-02-21 00:25:07 -0300

    Component Life Cycle vue-cli

--- hcode-soccer/src/components/HcodeSectionNews.vue
+++ hcode-soccer/src/components/HcodeSectionNews.vue
@@ -2,40 +2,15 @@
         <section>
             <div class="container">
                 <HcodeSectionNewsIndividual 
-                    img-name="news1.jpg"
-                    img-info = "Notícia 1"
-                    news-title="Começam os treinos para a nova temporada"
-                    news-content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error soluta 
-                    quibusdam quia esse iure tenetur nisi? Sint maiores reprehenderit nesciunt vel, maxime 
-                    consectetur earum sequi, voluptates ab eos ullam provident? Lorem ipsum, dolor sit amet 
-                    consectetur adipisicing elit. Consectetur debitis vero dolores veniam neque, architecto ab 
-                    maxime odit ut. Perferendis quaerat explicabo eos nihil cumque sed, itaque incidunt quisquam aliquid!"
-                    news-date="01/01/2020"
+                    v-for="notice in news"
+                    :key = "notice.id"
+                    :img-name="notice.img"
+                    :img-info = "notice.imgInfo"
+                    :news-title="notice.title"
+                    :news-content="notice.content"
+                    :news-date="notice.date"
                 />
 
-                <HcodeSectionNewsIndividual 
-                    img-name="news2.jpg"
-                    img-info = "Notícia 2"
-                    news-title="Jogo de quarta-feira termina empatado"
-                    news-content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error soluta 
-                    quibusdam quia esse iure tenetur nisi? Sint maiores reprehenderit nesciunt vel, maxime 
-                    consectetur earum sequi, voluptates ab eos ullam provident? Lorem ipsum, dolor sit amet 
-                    consectetur adipisicing elit. Consectetur debitis vero dolores veniam neque, architecto ab 
-                    maxime odit ut. Perferendis quaerat explicabo eos nihil cumque sed, itaque incidunt quisquam aliquid!"
-                    news-date="07/01/2020"
-                />
-
-                <HcodeSectionNewsIndividual 
-                    img-name="news3.jpg"
-                    img-info = "Notícia 3"
-                    news-title="A inauguração do novo estádio"
-                    news-content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error soluta 
-                    quibusdam quia esse iure tenetur nisi? Sint maiores reprehenderit nesciunt vel, maxime 
-                    consectetur earum sequi, voluptates ab eos ullam provident? Lorem ipsum, dolor sit amet 
-                    consectetur adipisicing elit. Consectetur debitis vero dolores veniam neque, architecto ab 
-                    maxime odit ut. Perferendis quaerat explicabo eos nihil cumque sed, itaque incidunt quisquam aliquid!"
-                    news-date="20/01/2020"
-                />
             </div>
         </section>
 </template>
@@ -46,7 +21,37 @@ import HcodeSectionNewsIndividual from './HcodeSectionNewsIndividual';
 export default {
     components:{
         HcodeSectionNewsIndividual
-    }
+    },
+    data() {
+        return {
+            news: []
+        }
+    },
+    created() {
+        this.news = [{
+            id: 1,
+            title: 'Começam os treinos para a nova temporada',
+            content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error soluta quibusdam quia esse iure tenetur nisi? Sint maiores reprehenderit nesciunt vel, maxime consectetur earum sequi, voluptates ab eos ullam provident? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur debitis vero dolores veniam neque, architecto ab maxime odit ut. Perferendis quaerat explicabo eos nihil cumque sed, itaque incidunt quisquam aliquid!',
+            date: '2020-01-01',
+            img: 'news1.jpg',
+            imgInfo: 'Notícia 1'
+
+        },{
+            id: 2,
+            title: 'Jogo de quarta-feira termina empatado',
+            content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error soluta quibusdam quia esse iure tenetur nisi? Sint maiores reprehenderit nesciunt vel, maxime consectetur earum sequi, voluptates ab eos ullam provident? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur debitis vero dolores veniam neque, architecto ab maxime odit ut. Perferendis quaerat explicabo eos nihil cumque sed, itaque incidunt quisquam aliquid!',
+            date: '2020-01-07',
+            img: 'news2.jpg',
+            imgInfo: 'Notícia 2'
+        }, {
+            id: 3,
+            title: 'A inauguração do novo estádio',
+            content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error soluta quibusdam quia esse iure tenetur nisi? Sint maiores reprehenderit nesciunt vel, maxime consectetur earum sequi, voluptates ab eos ullam provident? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur debitis vero dolores veniam neque, architecto ab maxime odit ut. Perferendis quaerat explicabo eos nihil cumque sed, itaque incidunt quisquam aliquid!',
+            date: '2020-01-22',
+            img: 'news3.jpg',
+            imgInfo: 'Notícia 3'
+        }]
+    },
 }
 </script>
 
