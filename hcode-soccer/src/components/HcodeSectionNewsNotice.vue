0d253a1897956f4045c9ffca77eb811af4c1ecc0
Wesley Wirmond <wesleywirmond@gmail.com>
2021-02-27 01:42:47 -0300

    methods to navigation Vue-Router

---
+++ hcode-soccer/src/components/HcodeSectionNewsNotice.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="section-news">
+        <div class="container">
+            <div class="row div-news">
+                <div class="col-6">
+                    <img src="../assets/news1.jpg" alt="">
+                </div>
+                <div class="col-6">
+                    <h2>Título</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora delectus et consequuntur quod qui possimus quos? Dolores asperiores velit, minima ex explicabo officiis rem. Provident nobis accusantium maxime repudiandae delectus!</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+h2{
+    cursor: auto;
+    color: #000;
+}
+</style>
