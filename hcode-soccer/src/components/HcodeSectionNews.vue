b5dca293e624af0b9218f29b9ede9da45767d331
Wesley Wirmond <wesleywirmond@gmail.com>
2021-02-27 02:31:41 -0300

    names to the routes Vue-Router

--- hcode-soccer/src/components/HcodeSectionNews.vue
+++ hcode-soccer/src/components/HcodeSectionNews.vue
@@ -9,7 +9,9 @@
                     :news-date="notice.date"
                 >
                     <template #title>
-                        <h2 @click="goToPage(`/news/${notice.id}`)">{{ notice.title }}</h2>
+                        <router-link :to="{ name: 'notice', params: {idnotice: notice.id} }" tag="h2">
+                            {{ notice.title }}
+                        </router-link>
                     </template>
                         <p>{{ notice.content | truncate(200) }}</p>
                 </HcodeSectionNewsIndividual>
@@ -37,9 +39,9 @@ export default {
         })
     },
     methods: {
-        goToPage(page){
-            this.$router.push(page);
-        }
+        // goToPage(page){
+        //     this.$router.push(page);
+        // }
     },
 }
 </script>
